d776c693e8ccce33add84b0f039c55caa5573c16
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-08-19 06:25:37 +0100

    Remove godotenv

--- api/server.go
+++ api/server.go
@@ -1,25 +1,22 @@
 package api
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/RaphaelNagato/goresume-api/api/controllers"
 	"github.com/RaphaelNagato/goresume-api/api/seed"
-	"github.com/joho/godotenv"
 )
 
 var server = controllers.Server{}
 
 func Run() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error getting env, not coming through %v", err)
-	} else {
-		fmt.Println("We are getting the env values")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error getting env, not coming through %v", err)
+	// } else {
+	// 	fmt.Println("We are getting the env values")
+	// }
 
 	server.Initialize("postgres", os.Getenv("DATABASE_URL"))
 
