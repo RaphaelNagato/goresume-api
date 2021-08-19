f073c89cfe1fec533e3133d351ea6f07298dcb87
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-08-19 07:20:13 +0100

    modify model

--- api/models/contact.go
+++ api/models/contact.go
@@ -8,6 +8,7 @@ import (
 //Message Struct(Model)
 type ContactMe struct {
 	ID          string
+	Title       string `gorm:"not null" json:"title"`
 	Subject     string `gorm:"not null" json:"subject"`
 	Message     string `gorm:"not null" json:"message"`
 	SenderEmail string `gorm:"not null" json:"sender"`
